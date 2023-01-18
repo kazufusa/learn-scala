ffdc7fc350e3a56924874a23045c7bce97f2041b
kazufusa <kazu.jiyuunosenshi.fusa@gmail.com>
2023-01-18 22:17:48 +0900

    feat(zio): initial commit

---
+++ 20230118_learn_zio/src/main/scala/Main.scala
@@ -0,0 +1,29 @@
+import zio._
+
+import java.io.IOException
+
+object MainApp extends ZIOAppDefault {
+  // val myApp: ZIO[Any, IOException, Unit] =
+  //   for {
+  //     rnd <- Random.nextIntBounded(100)
+  //     _   <- Console.printLine(s"Random number: $rnd")
+  //     _   <- Clock.sleep(1.second)
+  //   } yield ()
+  //
+  // def run = myApp.forever
+
+  // def run = for {
+  //   _ <- Console.print("Please enter your name: ")
+  //   name <- Console.readLine
+  //   < <- Console.print(s"Hello, $name!")
+  // } yield ()
+
+  // def run = Console.print("Please enter your name: ")
+  //   .flatMap { _ => 
+  //     Console.readLine
+  //       .flatMap { name =>
+  //         Console.print(s"Hello $name!")
+  //       }
+  //   }
+
+}
