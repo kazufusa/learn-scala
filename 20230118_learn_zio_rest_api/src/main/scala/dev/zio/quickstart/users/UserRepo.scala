c977b918a4555a20eca8435bc43c98445151c62a
kazufusa <kazu.jiyuunosenshi.fusa@gmail.com>
2023-02-04 02:07:54 +0900

    resolve warning

--- 20230118_learn_zio_rest_api/src/main/scala/dev/zio/quickstart/users/UserRepo.scala
+++ 20230118_learn_zio_rest_api/src/main/scala/dev/zio/quickstart/users/UserRepo.scala
@@ -12,5 +12,5 @@ trait UserRepo {
 object UserRepo {
   def register(user: User): ZIO[UserRepo, Throwable, String] = ZIO.serviceWithZIO[UserRepo](_.register(user))
   def lookup(id: String): ZIO[UserRepo, Throwable, Option[User]] = ZIO.serviceWithZIO[UserRepo](_.lookup(id))
-  def users(): ZIO[UserRepo, Throwable, List[User]] = ZIO.serviceWithZIO[UserRepo](_.users)
+  def users(): ZIO[UserRepo, Throwable, List[User]] = ZIO.serviceWithZIO[UserRepo](_.users())
 }
