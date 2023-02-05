52a5fb5908e00e190ca200bfb37772f78e0882c3
kazufusa <kazu.jiyuunosenshi.fusa@gmail.com>
2023-02-05 21:54:32 +0900

    feat(zio,config): initial commit

---
+++ 20230205_learn_zio_application_configuration/build.sbt
@@ -0,0 +1,19 @@
+ThisBuild / version := "0.1.0-SNAPSHOT"
+
+ThisBuild / scalaVersion := "2.13.10"
+
+lazy val root = (project in file("."))
+  .settings(
+    name := "learn_zio_rest_api_application_configuration"
+  )
+
+libraryDependencies ++= Seq(
+  "org.scalatest"  %% "scalatest"      % "3.2.15" % "test",
+  "dev.zio"        %% "zio"            % "2.0.0-RC6",
+  "dev.zio"        %% "zio-json"       % "0.3.0-RC8",
+  "io.d11"         %% "zhttp"          % "2.0.0-RC9",
+  "io.d11"         %% "zhttp-test"     % "2.0.0-RC8",
+  "io.getquill"    %% "quill-zio"      % "3.17.0-RC2",
+  "io.getquill"    %% "quill-jdbc-zio" % "3.17.0-RC2",
+  "com.h2database" %  "h2"             % "2.1.212"
+)
---
+++ 20230205_learn_zio_application_configuration/src/main/scala/dev/zio/quickstart/download/DownloadApp.scala
@@ -0,0 +1,43 @@
+package dev.zio.quickstart.download
+
+import zhttp.http._
+import zio.stream.ZStream
+import zio._
+
+import java.io.File
+
+/**
+ * An http app that:
+ *   - Accepts a `Request` and returns a `Response`
+ *   - Does not fail
+ *   - Does not use the environment
+ */
+object DownloadApp {
+  def apply(): Http[Any, Throwable, Request, Response] =
+    Http.collectHttp[Request] {
+      case Method.GET -> !! / "download" =>
+        val fileName = "file.txt"
+        Http
+          .fromStream(ZStream.fromResource(fileName))
+          .setHeaders(
+            Headers(
+              ("Content-Type", "application/octet-stream"),
+              ("Content-Disposition", s"attachmenet: filename=${fileName}")
+            )
+          )
+      case Method.GET -> !! / "download" / "stream" =>
+        val fileName = "bigfile.txt"
+        Http
+          .fromStream(
+            ZStream
+              .fromResource(fileName)
+              .schedule(Schedule.spaced(50.millis))
+          )
+          .setHeaders(
+            Headers(
+              ("Content-Type", "application/octet-stream"),
+              ("Content-Disposition", s"attachmenet: filename=${fileName}")
+            )
+          )
+    }
+}
