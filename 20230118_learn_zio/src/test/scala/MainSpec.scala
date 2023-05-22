dac0167db565959bb01403a2608df8037de0be81
kazufusa <kazu.jiyuunosenshi.fusa@gmail.com>
2023-05-22 22:05:27 +0900

    test: 💍 ZIO: add error handling

--- 20230118_learn_zio/src/test/scala/MainSpec.scala
+++ 20230118_learn_zio/src/test/scala/MainSpec.scala
@@ -10,6 +10,30 @@ class MainSpec extends AnyFlatSpec with Matchers with ScalaFutures {
     actual.futureValue shouldBe "hello"
   }
 
+  "" should "" in {
+    val actual: Task[Int] =
+      // ZIO.fail(new Exception("1")).flatMap(_ => ZIO.succeed(1))
+      for {
+        _ <- ZIO.fail(new Exception("1"))
+        actual <- ZIO.succeed(1)
+      } yield actual
+    TestUtil.futureFromZIO(actual).failed.futureValue shouldBe an[Exception]
+  }
+
+  "Left(error)" should "be ZIO.fail" in {
+    val expected = new Exception("some error")
+    val input: Either[Exception, Int] = Left(expected)
+    val actual = ZIO.fromEither(input).map(v => v * 2)
+    TestUtil.futureFromZIO(actual).failed.futureValue shouldBe expected
+  }
+
+  "Right(1)" should "be ZIO.succeed(2)" in {
+    val expected = 2
+    val input: Either[Exception, Int] = Right(1)
+    val actual = ZIO.fromEither(input).map(v => v * 2)
+    TestUtil.futureFromZIO(actual).futureValue shouldBe expected
+  }
+
   private def foldTest(flg: Boolean) =
     if (flg) ZIO.fail(new Exception("error")) else ZIO.succeed("hello")
 
